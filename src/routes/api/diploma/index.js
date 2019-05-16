7958a22b424e95b6d990fc8938eead8e3db071fa
Cuong Nguyen <cuong079816@gmail.com>
2019-05-16 14:19:24 +0700

    a

--- src/routes/api/diploma/index.js
+++ src/routes/api/diploma/index.js
@@ -1,17 +1,17 @@
-import express from "express";
-import fs from "fs";
-import pdfMakePrinter from "pdfmake/src/printer";
-import TemplatePDF from "./templatePDF";
+import express from 'express';
+import fs from 'fs';
+import pdfMakePrinter from 'pdfmake/src/printer';
+import TemplatePDF from './templatePDF';
 
 const router = express.Router();
-router.post("/create-diploma", (req, res) => {
+router.post('/create-diploma', (req, res) => {
   try {
     const fonts = {
       Roboto: {
-        normal: "fonts/Roboto-Regular.ttf",
-        bold: "fonts/Roboto-Medium.ttf",
-        italics: "fonts/Roboto-Italic.ttf",
-        bolditalics: "fonts/Roboto-MediumItalic.ttf"
+        normal: 'fonts/Roboto-Regular.ttf',
+        bold: 'fonts/Roboto-Medium.ttf',
+        italics: 'fonts/Roboto-Italic.ttf',
+        bolditalics: 'fonts/Roboto-MediumItalic.ttf'
       }
     };
     let creatStream;
@@ -19,22 +19,22 @@ router.post("/create-diploma", (req, res) => {
     const printer = new pdfMakePrinter(fonts);
     const doc = printer.createPdfKitDocument(docDefinition);
     doc.pipe(
-      (creatStream = fs.createWriteStream("pdfs/test.pdf").on("error", err => {
+      (creatStream = fs.createWriteStream('pdfs/test.pdf').on('error', err => {
         if (err) {
           res.status(500).send(JSON.stringify(err));
         }
       }))
     );
-    doc.on("end", () => {
-      res.send({ message: "DONE" });
+    doc.on('end', () => {
+      res.send({ message: 'DONE' });
     });
     doc.end();
   } catch (err) {
     throw err;
   }
 });
-router.get("/download", (req, res) => {
-  res.download("pdfs/test.pdf", err => {
+router.get('/download', (req, res) => {
+  res.download('pdfs/test.pdf', err => {
     res.status(500);
   });
 });
