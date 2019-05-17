a1d7b8c6ddaa3e12862f3864c0f2c92546e53567
Cuong Nguyen <cuong079816@gmail.com>
2019-05-17 23:51:21 +0700

    a

---
+++ src/config/rsaKey/index.js
@@ -0,0 +1,16 @@
+import cryptico from 'cryptico-js';
+
+class RSAKey {
+  init = address => {
+    this.address = address;
+  };
+  generateKeyPair = () => {
+    if (this.address) {
+      this.RSAKey = cryptico.generateRSAKey(address, 1024);
+    }
+  };
+  getKeyPair = () => {
+    return this.RSAKey;
+  };
+}
+export default new RSAKey();
